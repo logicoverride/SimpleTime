69b2a15cbeac4596dce512e10a1af5827cd2a4ad
LogicOverride <logicoverride@tutanota.com>
2016-08-23 21:17:38 +1000

    Skeleton

---
+++ SimpleTimeBL/Services/ServiceFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTimeBL.Services
+{
+    public sealed class ServiceFactory
+    {
+        #region Fields
+
+        private static readonly ServiceFactory _factory = new ServiceFactory();
+
+        #endregion
+
+        #region Properties
+
+        public static ServiceFactory Factory
+        {
+            get { return _factory; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        static ServiceFactory()
+        {
+        }
+
+        ServiceFactory()
+        {
+        }
+
+        #endregion
+    }
+}
